aa3fece335f6b40dac822b1efb5b426e736b3ba1
Karthikeyan1993 <karthik.infotec99@gmail.com>
2020-06-05 23:43:02 +0530

    config changes

--- src/main/java/com/karthik/springcloudconfigclient/SpringCloudConfigClientApplication.java
+++ src/main/java/com/karthik/springcloudconfigclient/SpringCloudConfigClientApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringCloudConfigClientApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudConfigClientApplication.class, args);
 	}
